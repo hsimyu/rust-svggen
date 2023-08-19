0a15d54e8b622a4edc219e62dc816e468a84fb2c
hsimyu <hsimyu1900@gmail.com>
2023-08-20 01:01:32 +0900

    First impl.

---
+++ src/lib.rs
@@ -0,0 +1,118 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct SvgBuilder {
+    content: String,
+}
+
+impl SvgBuilder {
+    pub fn new() -> SvgBuilder {
+        SvgBuilder {
+            content: String::new(),
+        }
+    }
+
+    pub fn begin(&mut self) {
+        self.content.push_str(
+        r#"<svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+"#);
+    }
+
+    pub fn rect<'a>(&'a mut self, width: &'a str, height: &'a str) -> RectBuilder<'a> {
+        RectBuilder {
+            parent: self,
+            width,
+            height,
+        }
+    }
+
+    pub fn circle<'a>(&'a mut self, cx: u32, cy: u32, radius: u32) -> CircleBuilder<'a> {
+        CircleBuilder {
+            parent: self,
+            cx,
+            cy,
+            radius,
+        }
+    }
+
+    pub fn text<'a>(
+        &'a mut self,
+        x: u32,
+        y: u32,
+        font_size: u32,
+        text: &'a str,
+    ) -> TextBuilder<'a> {
+        TextBuilder {
+            parent: self,
+            x,
+            y,
+            font_size,
+            text,
+        }
+    }
+
+    pub fn end(&mut self) {
+        self.content.push_str(r#"</svg>"#);
+    }
+
+    pub fn save_as(&self, file_name: &str) -> Result<(), Box<dyn Error>> {
+        let mut f = File::create(file_name)?;
+        f.write_all(self.content.as_bytes())?;
+        Ok(())
+    }
+}
+
+pub struct RectBuilder<'a> {
+    parent: &'a mut SvgBuilder,
+    width: &'a str,
+    height: &'a str,
+}
+
+impl Drop for RectBuilder<'_> {
+    fn drop(&mut self) {
+        let elem = format!(
+            "<rect width=\"{:}\" height=\"{:}\" />\n",
+            self.width, self.height
+        );
+        self.parent.content.push_str(&elem);
+    }
+}
+
+pub struct CircleBuilder<'a> {
+    parent: &'a mut SvgBuilder,
+    cx: u32,
+    cy: u32,
+    radius: u32,
+}
+
+impl Drop for CircleBuilder<'_> {
+    fn drop(&mut self) {
+        let elem = format!(
+            "<circle cx=\"{:}\" cy=\"{:}\" radius=\"{:}\" />\n",
+            self.cx, self.cy, self.radius
+        );
+        self.parent.content.push_str(&elem);
+    }
+}
+
+pub struct TextBuilder<'a> {
+    parent: &'a mut SvgBuilder,
+    x: u32,
+    y: u32,
+    font_size: u32,
+    text: &'a str,
+}
+
+impl Drop for TextBuilder<'_> {
+    fn drop(&mut self) {
+        let mut elem = format!(
+            "<text x=\"{:}\" y=\"{:}\" font-size=\"{:}\" text-anchor=\"middle\" fill=\"white\">",
+            self.x, self.y, self.font_size
+        );
+        elem += self.text;
+        elem += "</text>\n";
+
+        self.parent.content.push_str(&elem);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate svggen;
+
+fn main() {
+    let mut builder = svggen::SvgBuilder::new();
+
+    builder.begin();
+
+    builder.rect("100%", "100%");
+    builder.circle(150, 100, 80);
+    builder.text(150, 125, 60, "SVG");
+
+    builder.end();
+
+    builder.save_as("myimage.svg").unwrap();
+}
